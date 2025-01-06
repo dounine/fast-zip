fa1fb3a09702d471ba4c53a2b34228fcae252945
lake <102535481@qq.com>
2025-01-07 04:18:12 +0800

    eocd范围读取，允许zip文件添加注释

--- src/eocd.rs
+++ src/eocd.rs
@@ -1,8 +1,9 @@
-use crate::stream::endian::Endian;
+use crate::magic::Magic;
 use crate::stream::bytes::ValueRead;
+use crate::stream::len::Len;
+use crate::stream::pin::Pin;
 use crate::stream::stream::Stream;
 use std::io::{Read, Seek, SeekFrom, Write};
-use crate::magic::Magic;
 
 #[derive(Debug)]
 pub struct EoCd {
@@ -14,18 +15,53 @@ pub struct EoCd {
     pub offset: u32,
     pub comment_length: u16,
 }
+impl EoCd {
+    pub fn find_offset<T: Read + Write + Seek>(stream: &mut Stream<T>) -> std::io::Result<u64> {
+        let max_eocd_size: u64 = u16::MAX as u64 + 22;
+        let mut search_size: u64 = 22; //最快的搜索
+        let file_size = stream.len()?;
 
-impl<T: Read + Write + Seek> ValueRead<T> for EoCd {
-    fn read(stream: &mut Stream<T>) -> std::io::Result<Self> {
-        stream.seek(SeekFrom::End(-22))?;
-
-        let magic: Magic = stream.read_value()?;
-        if magic != Magic::EoCd {
+        if file_size < search_size {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::InvalidData,
                 "not a zip file",
             ));
         }
+        let eocd_magic: u32 = Magic::EoCd.into();
+        loop {
+            // 确保搜索范围不超过 EOCD 的最大大小
+            search_size = search_size.min(max_eocd_size);
+            stream.seek(SeekFrom::End(-(search_size as i64)))?;
+            for i in 0..search_size - 3 {
+                stream.pin()?;
+                let magic: u32 = stream.read_value()?;
+                stream.un_pin()?;
+                stream.seek(SeekFrom::Current(1))?;
+                if magic == eocd_magic {
+                    return Ok(search_size - i);
+                }
+                if search_size == 22 {
+                    break;
+                }
+            }
+            if search_size >= max_eocd_size {
+                break;
+            }
+            search_size = (search_size * 2).min(file_size);
+        }
+
+        Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "not a zip file",
+        ))
+    }
+}
+
+impl<T: Read + Write + Seek> ValueRead<T> for EoCd {
+    fn read(stream: &mut Stream<T>) -> std::io::Result<Self> {
+        let eocd_offset = Self::find_offset(stream)?;
+        stream.seek(SeekFrom::End(-(eocd_offset as i64)))?;
+        stream.seek(SeekFrom::Current(4))?;
 
         Ok(EoCd {
             number_of_disk: stream.read_value()?,
